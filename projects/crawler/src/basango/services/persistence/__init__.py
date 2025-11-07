0f6d7f3697467f591f2096dbb921bbad8cb3f5de
bernard-ng <ngandubernard@gmail.com>
2025-11-07 13:30:19 +0200

    [crawler] remove python implementation

--- projects/crawler/src/basango/services/persistence/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_persistor import BasePersistor
-from .csv_persistor import CsvPersistor
-from .json_persistor import JsonPersistor
-
-__all__ = [
-    "BasePersistor",
-    "CsvPersistor",
-    "JsonPersistor",
-]
--- projects/crawler/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-# Ensure 'src' is on sys.path so `import basango...` works in tests
-ROOT = os.path.dirname(os.path.dirname(__file__))
-SRC = os.path.join(ROOT, "src")
-if SRC not in sys.path:
-    sys.path.insert(0, SRC)
