27e791f3b1ca2c1ffd1ab7989dcaea7189020253
bernard-ng <ngandubernard@gmail.com>
2025-11-11 13:50:55 +0200

    feat(ui): add primitive components

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@basango/ui/components/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,14 +51,7 @@ export default function Home() {
             />
             Deploy Now
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+          <Button variant="secondary">test component</Button>
         </div>
       </main>
     </div>
